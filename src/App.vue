135920463c51c2fe5c93f536e5bdb4ddec485f1d
Nabil5352 <nabil@tgalimited.com>
2018-09-17 18:52:36 +0600

    coin base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-background/>
+  </div>
+</template>
+
+<script>
+import HomeBackground from './components/HomeBG.vue'
+
+export default {
+	components: {
+		HomeBackground
+	}
+}
+</script>
+
+<style lang="scss">
+	@import '../assets/scss/app.scss';
+</style>
---
+++ src/components/HomeBG.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home">
+    <div id="home-bg">
+      <img src="assets/img/home-bg.svg">
+    </div>
+
+    <div class="form">
+      <div class="text-left"><h1>Coin Base</h1></div>
+      <div class="input-group">
+        <label>Username:</label>
+        <input
+          type="text"
+          placeholder="Enter your name">
+      </div>
+      <div class="input-group">
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder="Enter your password">
+      </div>
+      <div class="sign-in-note text-left">asdadasdasdad</div>
+      <div class="text-left"><button class="btn btn-primary">Sign In</button></div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
