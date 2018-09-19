3124eff807618445aa8a3d2b40b3316db5434aba
Nabil5352 <nabil@tgalimited.com>
2018-09-19 15:51:27 +0600

    updated design

--- src/layout/partial/Content.vue
+++ src/layout/partial/Content.vue
@@ -18,15 +18,21 @@
               <div class="wrapper">
                 <div class="title">Main</div>
                 <ul>
-                  <li
-                    :class="{ active : isActiveClass(1) }"
-                    @click="setActiveLink(1)"><router-link :to="{name: 'dashboard'}"><span><img src="/assets/img/currency.svg"></span>&nbsp;&nbsp;<span>Current Price</span></router-link></li>
-                  <li
-                    :class="{ active : isActiveClass(2) }"
-                    @click="setActiveLink(2)"><router-link :to="{name: 'personal'}"><span><img src="/assets/img/user.png"></span>&nbsp;&nbsp;<span>Your Currency</span></router-link></li>
-                  <li
-                    :class="{ active : isActiveClass(3) }"
-                    @click="setActiveLink(3)"><router-link :to="{name: 'history'}"><span><img src="/assets/img/calender.png"></span>&nbsp;&nbsp;<span>Last 31 days</span></router-link></li>
+                  <router-link :to="{name: 'dashboard'}">
+                    <li
+                      key="1"
+                      :class="{ active : isActiveClass(1) }"
+                      @click="setActiveLink(1)"><span><img src="/assets/img/currency.svg"></span>&nbsp;&nbsp;<span>Current Price</span></li></router-link>
+                  <router-link :to="{name: 'personal'}">
+                    <li
+                      key="2"
+                      :class="{ active : isActiveClass(2) }"
+                      @click="setActiveLink(2)"><span><img src="/assets/img/user.png"></span>&nbsp;&nbsp;<span>Your Currency</span></li></router-link>
+                  <router-link :to="{name: 'history'}">
+                    <li
+                      key="3"
+                      :class="{ active : isActiveClass(3) }"
+                      @click="setActiveLink(3)"><span><img src="/assets/img/calender.png"></span>&nbsp;&nbsp;<span>Last 31 days</span></li></router-link>
                 </ul>
               </div>
             </div><!--menu-->
@@ -34,9 +40,11 @@
               <div class="wrapper">
                 <div class="title">My Account</div>
                 <ul>
-                  <li
-                    :class="{ active : isActiveClass(4) }"
-                    @click="setActiveLink(4)"><router-link :to="{name: 'profile'}"><span><img src="/assets/img/user.png"></span>&nbsp;&nbsp;<span>Your Profile</span></router-link></li>
+                  <router-link :to="{name: 'profile'}">
+                    <li
+                      key="4"
+                      :class="{ active : isActiveClass(4) }"
+                      @click="setActiveLink(4)"><span><img src="/assets/img/user.png"></span>&nbsp;&nbsp;<span>Your Profile</span></li></router-link>
                   <li><span><img src="/assets/img/signout.png"></span>&nbsp;&nbsp;<span>Log Out</span></li>
                 </ul>
               </div>
@@ -45,11 +53,7 @@
           <div class="scroll-content right-box-content">
             <top-bar/>
             <div class="middle-content">
-              <transition
-                name="fade"
-                mode="out-in">
-                <router-view/>
-              </transition>
+              <router-view/>
             </div><!--middle-content-->
             <fixed-footer/>
           </div>
